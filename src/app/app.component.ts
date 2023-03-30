1d298efda21f55e8525384376a494a56fdfb2294
Ana Boca <Ana.Boca@asu.edu>
2023-03-30 11:43:35 -0700

    readme update

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hivehapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hivehapp';
+}
