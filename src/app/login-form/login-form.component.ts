07c40fa86d4e830946f0fe4eb6696d837d9ffa87
Ana Boca <Ana.Boca@asu.edu>
2023-03-31 14:32:09 -0700

    login form styles

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,12 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { Login } from '@hivehapp/models';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'hivehapp-login-form',
+  selector: 'login-form',
   standalone: true,
   imports: [
     CommonModule,
@@ -14,14 +16,34 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     FormsModule,
     MatInputModule,
+    MatButtonModule,
   ],
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
   @ViewChild('loginForm') form: NgForm;
 
-  onSave() {
-    console.log('boop');
+  model: Login;
+
+  ngOnInit() {
+    this.model = {
+      emailAddress: '',
+      password: '',
+    };
   }
+
+  onSave() {
+    if (this.form.valid) {
+      console.log('Saved!');
+    }
+  }
+}
+
+function deepCopy<T>(object: T): T {
+  if (object == null) {
+    return object;
+  }
+
+  return JSON.parse(JSON.stringify(object));
 }
